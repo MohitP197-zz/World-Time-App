b0f383855310336768f303006838be04d7bce68c
Mohit Paudel <39890575+MohitP197@users.noreply.github.com>
2020-04-06 11:59:41 +0545

    Made asynchronors function

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,12 +6,23 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getData() {
+  // If some data depends on another data from another request or function then we need to
+  // make the function synchronos and use the combination of asyncronous function (async) after parenthesis
+  // and before curly braces with await keyword
+  void getData() async {
     // Similate a network request for a username
 
-    Future.delayed(Duration(seconds: 3), () {
+    // After adding await, the function with await have to finish first, then only other function runs
+    await Future.delayed(Duration(seconds: 3), () {
       print('Mohit');
     });
+
+    // Simulate network request to get bio of the username
+    Future.delayed(Duration(seconds: 2), () {
+      print('Mohit, is a programmer');
+    });
+
+    print('statement');
   }
 
   @override
@@ -22,7 +33,6 @@ class _ChooseLocationState extends State<ChooseLocation> {
 
   @override
   Widget build(BuildContext context) {
-    print('This is the build');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
